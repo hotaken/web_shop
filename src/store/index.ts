cee280282c144b974f6f34c099e71cb922b820a3
hotaken <soland777@ya.ru>
2021-05-21 03:37:57 +0500

    feat: 🎸 throttle redux to local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { createStore, compose, combineReducers } from 'redux';
+import { throttle } from 'lodash';
+
 import ingredientsReducer from './ingredients';
 import { loadFromLocalStorage, saveToLocalStorage } from '../utils/localStorage';
 
 const LOCAL_STORAGE_REDUX_NAME = 'REDUX_INGREDIENTS';
+const LOCAL_STORAGE_THROTTLE_TIME = 2000;
 
 const composeEnhansers =
     process.env.NODE_ENV === 'development'
@@ -20,10 +23,12 @@ const store = createStore(
     composeEnhansers(),
 );
 
-store.subscribe(() => {
-    // подписали REDUX на запись в локальное хранилище при изменении данных.
-    saveToLocalStorage(store.getState(), LOCAL_STORAGE_REDUX_NAME);
-});
+store.subscribe(
+    throttle(() => {
+        // подписали REDUX на запись в локальное хранилище при изменении данных.
+        saveToLocalStorage(store.getState(), LOCAL_STORAGE_REDUX_NAME);
+    }, LOCAL_STORAGE_THROTTLE_TIME),
+);
 
 export type StoreType = ReturnType<typeof rootReducer>;
 export type StoreDispatchType = typeof store.dispatch;
