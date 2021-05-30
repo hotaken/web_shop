0e5b4ad3408c9b3f2a331ddf763d9a9c10570383
mchrome <artyom2800@yandex.ru>
2021-05-31 02:34:19 +0500

    feat: 🎸 ingredients api

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.get('/api', (req, res) => {
 console.log(__dirname);
 
 //Подключение к БД
-let db = new sqlite3.Database('server/database.sqlite', (err) => {
+let db = new sqlite3.Database(path.join('server/dataBase.sqlite'), (err) => {
     if (err) {
         console.log('-------------------------------');
         console.log(__dirname);
         console.log(err.message);
         return console.log(err.message);
     }
-    console.log('Connected to server/database.sqlite');
+    console.log('Connected to server/dataBase.sqlite');
     console.log(__dirname);
 });
 
@@ -27,6 +27,16 @@ app.get('/api/getBurgerList', function (request, response) {
     let array = [];
     let burger = request.query.burger;
     if (burger == 'All') {
+        let ingredients = {};
+        console.log('SELECT * FROM ingredients');
+        db.all('SELECT * FROM ingredients', (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            rows.forEach((row) => {
+                ingredients[row.name]=row.price
+            });
+        });
         console.log('SELECT * FROM burger_list');
         db.all('SELECT * FROM burger_list', (err, rows) => {
             if (err) {
@@ -37,12 +47,32 @@ app.get('/api/getBurgerList', function (request, response) {
                     name: row.name,
                     ingredients: JSON.parse(row.jsonIngredients)['ingredients'],
                     description: row.description,
+                    price: JSON.parse(row.jsonIngredients)['ingredients'].map((ingredient) => {
+                        return ingredients[ingredient]
+                    }).reduce((a,b)=>{
+                        return a + b;
+                    })
                 });
             });
             response.write(JSON.stringify(array));
             response.end();
         });
     }
+});
+
+app.get('/api/getIngredientPrices', function (request, response) {
+    let ingredients = {};
+    console.log('SELECT * FROM ingredients');
+    db.all('SELECT * FROM ingredients', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            ingredients[row.name]=row.price
+        });
+        response.write(JSON.stringify(ingredients));
+        response.end();
+    });
 });
 
 if (process.env.NODE_ENV === 'production') {
